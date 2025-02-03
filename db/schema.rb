cc1110983920e4b135f87c577a498e37f43bcc95
Rohith Raj <rohith.r@testyantra.in>
2025-02-03 18:06:33 +0530

    Added new tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_02_03_072042) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_03_091615) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -72,6 +72,13 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_03_072042) do
     t.index ["album_id"], name: "index_photos_on_album_id"
   end
 
+  create_table "statuses", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_statuses_on_name", unique: true
+  end
+
   create_table "sub_categories", force: :cascade do |t|
     t.string "name"
     t.datetime "deleted_at"
@@ -89,7 +96,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_03_072042) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "deleted_at"
-    t.string "timezone"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 

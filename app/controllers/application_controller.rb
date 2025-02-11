af7830ac870b7de0403b93dd0af823b4e24b7e59
Rohith Raj <rohith.r@testyantra.in>
2025-02-11 10:04:56 +0530

    Application controller changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,11 +12,4 @@ class ApplicationController < ActionController::API
         render json:{error: "Token Missing"},status: :unauthorized
       end
   end
-
-  def set_time_zone
-    token = request.headers[:authorization]
-    payload, _ = JwtToken.decode(token)
-    Time.zone = payload["timezone"]
-  end
-
 end

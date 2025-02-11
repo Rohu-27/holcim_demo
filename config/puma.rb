31efc5015a1035e2a191b6386b8975a021e7a3eb
Rohith Raj <rohith.r@testyantra.in>
2025-02-11 10:57:56 +0530

    Puma mutlithreading changes

--- config/puma.rb
+++ config/puma.rb
@@ -20,8 +20,7 @@ if rails_env == "production"
   # It defaults to 1 because it's impossible to reliably detect how many
   # CPU cores are available. Make sure to set the `WEB_CONCURRENCY` environment
   # variable to match the number of processors.
-  # worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { 1 })
-  worker_count = 2
+  worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { 1 })
   if worker_count > 1
     workers worker_count
   else
@@ -31,6 +30,12 @@ end
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
 # terminating a worker in development environments.
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
+worker_count = 2
+  if worker_count > 1
+    workers worker_count
+  else
+    preload_app!
+  end
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 port ENV.fetch("PORT") { 3000 }
